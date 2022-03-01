0859ab031680218647e50857a39654859d347867
이자훈 <jh.lee@break.co.kr>
2022-03-01 20:18:19 +0900

    Initial Commit

---
+++ Shared/WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewApp.swift
+//  Shared
+//
+//  Created by break on 2022/03/01.
+//
+
+import SwiftUI
+
+@main
+struct WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
